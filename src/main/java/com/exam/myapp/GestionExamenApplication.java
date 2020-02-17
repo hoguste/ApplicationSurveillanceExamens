5730a995ae28444b9d7f96c3f75c12a50da0295e
hoguste <hcasses@gmail.com>
2020-02-17 11:00:13 +0100

    PremierUploadDuProjet

---
+++ src/main/java/com/exam/myapp/GestionExamenApplication.java
@@ -0,0 +1,13 @@
+package com.exam.myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionExamenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionExamenApplication.class, args);
+	}
+
+}
